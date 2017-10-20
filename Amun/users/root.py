4671fdb6b6a59187dc1e04fc2a41d756057a291b
Aitch0R <31582708+Aitch0R@users.noreply.github.com>
2017-10-20 10:16:00 +0200

    Update root.py

--- Amun/users/root.py
+++ Amun/users/root.py
@@ -1,8 +1,9 @@
+import time
+
 configs=dict(name='root', usrid=1,admin=True, port=1000,rooms=[0]) ##################change userid to userID, change 1 to 0, rooms to all
 email='h.a.behery@gmail.com'
 Pass='*********'
 to=''
-import time
 
 def rule(self,msg):
 	print(msg)
@@ -12,10 +13,10 @@ def rule(self,msg):
 
 def init(self,protocols):
 	print('init')
-#	H=int(time.strftime('%H',time.localtime()))
+	H=int(time.strftime('%H',time.localtime()))
 #	m=int(time.strftime('%m',time.localtime()))
-#	if H>=6:
-#	      protocols.awake([0])
+	if H>=6:
+	      protocols.awake([0])
 	self.scheduler.scheduler.add_job(protocols.asleep, trigger='interval', start_date='2017-09-29 00:00:00', args=[self.rooms], days=1)
 	self.scheduler.scheduler.add_job(protocols.awake, trigger='interval', start_date='2017-09-29 6:30:00', args=[self.rooms], days=1)
 #	self.scheduler.scheduler.add_job(self.scheduler.users[0].output, trigger='interval', start_date='2017-09-29 13:52:05', args=['stuff'], minutes=1)
