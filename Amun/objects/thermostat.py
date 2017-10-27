123df77aac5af400e95766ca06447510e660b84c
Aitch0R <31582708+Aitch0R@users.noreply.github.com>
2017-10-27 23:41:42 +0200

    Update thermostat.py

--- Amun/objects/thermostat.py
+++ Amun/objects/thermostat.py
@@ -7,27 +7,50 @@ import infra.config as config
 obj=importlib.import_module(config.obj).amunobj
 
 logger = logging.getLogger('mainLogger')
-logger.info(':OK') #######################################################change name
+logger.info(':thermostat')
 #---------------------------------------------------------------------
 #relevants
 
-class newObj(obj):
+class thermostat(obj):
 	def __init__(self,parent,info,objid):
 		obj.__init__(self,parent,info,objid)
 		#zero------------------------------------------------
 		self.tStatus=0
-		self.status=0
+		self.aStatus=0
+		self.auto=0
 		#-----------------------------------------------------
 
 	def compose(self, to):
 		if to=='agentS':#setup
-			msg=self.index+',s,'################################+'#something
+			msg=self.index+',s,'#not needed till now
 		elif to=='agent':#cmd
-			msg=self.index+',c,'###############################+'#something
+			msg=self.index+','+str(self.tStatus)
 		elif to=='client':
-			msg=self.clientAddr+','+str(self.auto)+','######################+'#something
+			msg=self.clientAddr+','+str(self.auto)+','+str(self.aStatus)
 		return msg
-
+	
+	def setTStat(self,stat):
+		try:
+			if stat in ['0','1']:
+				self.tStat=stat
+				self.agent.objCmd(self.compose('agent'))
+		except ValueError:
+			pass
+		
+	def setAuto(self, stat):
+		try:
+			if stat in ['0','1']:
+				self.auto=stat
+		except ValueError:
+			pass
+		
+	def setAState(self,stat):
+		try:
+			if stat in ['0','1']:
+				self.aStat=stat
+		except ValueError:
+			pass
+		
 	def process (self, _input):
 		logger.debug(self.name +' process')
 		try:
@@ -40,4 +63,5 @@ class newObj(obj):
 
 	def aProcessor(self, _input):
 		#fill here
+		self.setAState
 		self.informAll()
